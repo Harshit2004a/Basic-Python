f042f9a08940e4db389bf22cb108960b2ade1829
Harshit Kumar <116547978+Harshit2004a@users.noreply.github.com>
2025-05-18 22:47:51 +0530

    Swap_list.py

--- Swap_list.py
+++ Swap_list.py
@@ -8,3 +8,5 @@ def swap_elements(lst, index1, index2):
 my_list = [1, 2, 3, 4]
 print("Original List:", my_list)
 print("Swap list:",(swap_elements(my_list, 1, 3)))  # Output: [1, 4, 3, 2]
+
+# Code by Harshit
